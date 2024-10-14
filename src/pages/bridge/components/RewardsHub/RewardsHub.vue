8472aa62edeeb8a46a49a711eec1892d8031e651
PerryTheShark <tungtv.uet@gmail.com>
2024-10-14 09:14:03 +0700

    add button rewards hub

--- src/pages/bridge/components/RewardsHub/RewardsHub.vue
+++ src/pages/bridge/components/RewardsHub/RewardsHub.vue
@@ -1,12 +1,20 @@
 <template>
     <OContainer>
-        <OHeader class="flex flex-col md:flex-row-reverse md:items-center md:pt-[40px]" title="_ REWARDS HUB"/>
-    </OContainer>
+            
+            <OHeader class="flex md:flex-row md:items-center md:justify-start" title="_ REWARDS HUB"> 
+                <div class="flex flex-col md:flex-row md:items-center mt-[20px] md:mt-0 md:mr-auto">
+                    <p class="mr-[8px] text-[14px] md:text-base text-[#a4a4a4] -tracking-tighter">YOUR ACCOUNT: </p>
+                    <WalletConnectButton />
+                </div>
+                
+            </OHeader>
+
+    </OContainer> 
 </template>
 
 <script setup lang="ts">
     import { OContainer, OHeader } from '@/components/OContainer'
-
+    import WalletConnectButton from 'D:/Fatpay/oooo-bridge-web/src/components/layout/WalletConnectButton.vue'
 
 </script>
 
